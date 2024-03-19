fde509ec2fb9eb9ddcf35357ef54f234788cf66c
kartik941 <kk6734401@gmail.com>
2024-03-19 19:16:57 +0530

    settings

--- Static/js/script.js
+++ Static/js/script.js
@@ -1,11 +1,3 @@
-'use strict';
-
-
-
-/**
- * add event on element
- */
-
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
