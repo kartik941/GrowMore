85cf950b3c77a9a7d7d93130da9551f4b4358fc0
kartik941 <kk6734401@gmail.com>
2024-03-19 21:48:41 +0530

    changes to be made

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -11,32 +11,6 @@ const addEventOnElem = function (elem, type, callback) {
 
 
 /**
- * navbar toggle
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const navToggler = document.querySelector("[data-nav-toggler]");
-
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
-  document.body.classList.toggle("active");
-}
-
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-  document.body.classList.remove("active");
-}
-
-addEventOnElem(navbarLinks, "click", closeNavbar);
-
-
-
-/**
  * header active
  */
 
