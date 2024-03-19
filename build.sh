73ab5f2dcb730c75b0e64eef8237a05f1f11c0dc
kartik941 <kk6734401@gmail.com>
2024-03-19 09:26:57 +0530

    edit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+pip install wheel
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+
