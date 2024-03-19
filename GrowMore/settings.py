2a8a2bf9594092e920eba91eb4699d901c4971fa
kartik941 <kk6734401@gmail.com>
2024-03-19 10:32:26 +0530

    db

--- GrowMore/settings.py
+++ GrowMore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lpj!m0u!k^@hjbadkaw@rt@4*b__wtyx)8xb&zjq5bx@9l9m^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
