b20326847620aa76ba671cb67697c4b4480a8445
kartik941 <kk6734401@gmail.com>
2024-03-19 20:09:52 +0530

    added databases

--- Home/views.py
+++ Home/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 import requests
+from .models import *
 
 
 def home(request):
     return render(request,"index.html")
 def about(request):
+
+        
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        email = request.POST.get("email")
+        desc = request.POST.get("message")
+        fed = feedback(name=name,phone = phone, email=email, msg=desc)
+        if [validate_email(email)]:
+            fed.save()
+            return render(request, "about.html")
+        else:
+            html = {'value':1}
+            return render(request, "about.html",)
+    
+
+
+
     return render(request,"about.html")
 def market(request):
     return render(request,"market.html")
